6d1857f82db37745453abe7406679d5672bff87f
fenix <fenix217grom@gmail.com>
2023-04-16 16:18:13 +0300

    24 задание выполнено

--- UNIT_2/homework/task22.py
+++ UNIT_2/homework/task22.py
@@ -11,15 +11,15 @@ text = input()
 print('Введите n:')
 n = int(input())
 
-def gen_alphabet (char_one, char_last, n):
+def gen_alphabet (char_first, char_last, n):
     alphabet = {}
-    char_one_dig = ord(char_one)
+    char_first_dig = ord(char_first)
     char_last_dig = ord(char_last)
-    for i in range(char_one_dig, char_last_dig + 1):
+    for i in range(char_first_dig, char_last_dig + 1):
         if i + n <= char_last_dig:
             alphabet[chr(i)] = chr(i+n)
         else:
-            alphabet[chr(i)] = chr(i + n - (char_last_dig - char_one_dig + 1))
+            alphabet[chr(i)] = chr(i + n - (char_last_dig - char_first_dig + 1))
     return alphabet
 
 def code (text, n):
